7300189d58b40fb5633c8177e2203f8f1962babc
Jaival <pateljaival05@gmail.com>
2024-04-30 17:55:58 -0400

    MyComponent React Component Implementation

--- src/MyComponent.tsx
+++ src/MyComponent.tsx
@@ -2,3 +2,30 @@ import React, {useCallback, useMemo} from 'react';
 
 import InfiniteLoader from './InfiniteLoader';
 import {useQueueState} from './useQueueState';
+
+
+interface MyComponentProps {
+    fetchData: () => Promise<any>;
+    initialData?: any[];
+    threshold?: number;
+}
+
+
+const MyComponent: React.FC<MyComponentProps> = ({fetchData, initialData = [], threshold=0.8}) => {
+    const [data, enqueueDataUpdate] = useQueueState(initialData);
+
+    const loadMore = useCallback(async () => {
+        const newData = await fetchData();
+        enqueueDataUpdate(prevData => [...prevData, ...newData]);
+    }, [enqueueDataUpdate, fetchData]);
+
+    const memoizedLoadMore = useMemo(() => loadMore, [loadMore]);
+
+
+    return (
+        <InfiniteLoader loadMore={memoizedLoadMore} threshold={threshold} />
+
+    )
+}
+
+export default MyComponent;
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export {default as MyComponent} from './MyComponent';
+export {useQueueState} from './useQueueState';
+export {default as InfiniteLoader} from './InfiniteLoader';
+
+
+
