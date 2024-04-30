8e8a053a63f05a27364f19cb168ce8dae4218171
Jaival <pateljaival05@gmail.com>
2024-04-30 16:59:50 -0400

    initial queue implementation for queue

---
+++ src/useQueueState.ts
@@ -0,0 +1,36 @@
+import { useState, useRef, useEffect, useCallback } from 'react';
+
+type UpdateFunction<T> = (prev: T) => T;
+
+export function useQueueState<T>(initialValue: T):[T, (updateFn: UpdateFunction<T>) => void] {
+    const [state, setState] = useState(<T>)(initialValue);
+    const queue = useRef<UpdateFunction<T>[]>([]);
+
+
+    const processQueue = useCallback(() => {
+        setState(prev => {
+            let newState = prev;
+            while (queue.current.length > 0) {
+                const update = queue.current.shift();
+                if (update) {
+                    newState = update(newState);
+                }
+            }
+
+            return newState;
+        })
+    }, []);
+
+    useEffect(() => {
+        if (queue.current.length >0 ) {
+            setTimeout(processQueue, 0);
+        }
+    }, [processQueue]);
+
+    const enqueueState = (updateFn: UpdateFunction<T>) => {
+        queue.current.push(updateFn);
+    }
+
+
+    return [state, enqueueState];
+}
