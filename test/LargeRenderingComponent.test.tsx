4cfa05d99c6426aeadf1c927d41ebe245350d531
Jaival <pateljaival05@gmail.com>
2024-05-03 00:26:58 -0400

    testing filed on markdown

--- test/LargeRenderingComponent.test.tsx
+++ test/LargeRenderingComponent.test.tsx
@@ -1,65 +1,64 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import fs from 'fs';
 import MyComponent from '../src/MyComponent';
-
 
 type TestComponent = React.FC;
 
-
 const components: TestComponent[] = Array.from({ length: 100 }, (_, i) => {
-  return () => (
-    <div style={{ color: `#${i}${i}${i}` }}>
-      <h1>{`Test Component ${i + 1}`}</h1>
-      <a href={`https://example.com/${i + 1}`}>Visit Test Component {i + 1}</a>
-      <img src="image.jpg" alt="Image" />;
-    </div>
-  );
+    return () => (
+        <div style={{ color: `#${i}${i}${i}` }}>
+            <h1>{`Test Component ${i + 1}`}</h1>
+            <a href={`https://example.com/${i + 1}`}>Visit Test Component {i + 1}</a>
+            <img src="image.jpg" alt="Image" />;
+        </div>
+    );
 });
 
+const measureRenderingTime = (component: React.ReactElement): number => {
+    const start = performance.now();
+    shallow(component as any); // Cast the component to 'any' type
+    const end = performance.now();
+    return end - start;
+};
 
-function renderData(data: React.ReactNode) {
-    if (typeof data === 'string' || typeof data === 'number') {
-        return <p>{data}</p>;
-    } else {
-        return data;
-    }
-}
-
-
-let count = 0;
-let norm_component = 0 
-let mycomponent = 0
-
+const renderingData:any = [];
 
 describe('Test Components', () => {
+    let componentsTime = 0;
+    let myComponentsTime = 0;
     components.forEach((Component, index) => {
         it(`renders TestComponent${index + 1} without crashing`, () => {
             const renderingTime = measureRenderingTime(<Component />);
-            console.log(`Rendering Time for TestComponent${index + 1}: ${renderingTime} milliseconds`);
-
-            console.log('----------------MyComponent Renders------------');
             const renderingTimeMyComponent = measureRenderingTime(<MyComponent fetchData={() => Promise.resolve()}><Component /></MyComponent>);
-            console.log(`Rendering Time of Component with MyComponent: ${renderingTimeMyComponent}`);
 
-            norm_component += renderingTime
-            mycomponent += renderingTimeMyComponent 
-            count += 1;
-           
+            renderingData.push({
+                component: `TestComponent${index + 1}`,
+                normalRenderingTime: renderingTime,
+                myComponentRenderingTime: renderingTimeMyComponent
+            });
+            componentsTime += renderingTime
+            myComponentsTime += renderingTimeMyComponent
+            console.log(`Rendering Time for TestComponent${index + 1}: ${renderingTime} milliseconds`);
+            console.log(`Rendering Time of Component with MyComponent: ${renderingTimeMyComponent} milliseconds`);
         });
     });
 
     afterAll(() => {
-        const averagePercentageIncrease = ((norm_component - mycomponent) / norm_component ) * 100;
-        console.log(`Average Percentage Increase of Component Rendering with MyComponent: ${averagePercentageIncrease}%`);
-    });
+
+        const markdown = [
+            '# Rendering Times for Native Data of Normal Components vs Spitha',
+            '| Component | Time (milliseconds) |',
+            '|-----------|---------------------|'
+          ];
+          renderingData.forEach((data: { component: string; normalRenderingTime: string; myComponentRenderingTime: string; }) => {
+            markdown.push(`| ${data.normalRenderingTime} | ${data.myComponentRenderingTime} |`);
+          });
+          markdown.push(`Average Percentage Increase: ${((componentsTime - myComponentsTime ) / (componentsTime)) * 100}`)
+      
+          fs.writeFileSync('RenderingComponentDataTest10.md', markdown.join('\n'));
+          console.log('Markdown file created with rendering times.');
+        });
+
+
 });
-
-
-
-
-const measureRenderingTime = (component: React.ReactElement) => {
-    const start = performance.now();
-    shallow(component as any); 
-    const end = performance.now();
-    return end - start;
-};
