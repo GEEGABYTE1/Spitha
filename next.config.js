9def9c790d62705e9345837383114f79c5542c18
Jaival <pateljaival05@gmail.com>
2024-05-03 20:34:47 -0400

    next publishment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
     webpack: (config, options) => {
       config.module.rules.push({
         test: /\.tsx?$/,
+        exclude: /node_modules\/(?!spitha)/,
         use: [
           options.defaultLoaders.babel,
           { loader: 'ts-loader', options: { transpileOnly: true } },
