50ec0d79cf3df4d3019ed46e4d5c27b7679ee9da
Jaival <pateljaival05@gmail.com>
2024-05-03 20:25:44 -0400

    jsx file?

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export {default as Spitha} from './MyComponent';
-//export {useQueueState} from './useQueueState';
-//export {default as InfiniteLoader} from './InfiniteLoader';
-
-
-
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+export {default as Spitha} from './MyComponent';
+//export {useQueueState} from './useQueueState';
+//export {default as InfiniteLoader} from './InfiniteLoader';
+
+
+
