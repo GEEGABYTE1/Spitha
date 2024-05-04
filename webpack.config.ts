04d8de700ee0ee3aa549ea8c02524cd61a597283
Jaival <pateljaival05@gmail.com>
2024-05-03 21:02:07 -0400

    webpack impl

---
+++ webpack.config.ts
@@ -0,0 +1,30 @@
+import path from "path";
+import { Configuration } from "webpack";
+
+const config: Configuration = {
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-typescript"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  
+};
+
+export default config;
